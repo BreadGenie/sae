d69bc1824387a7d34b7d2698c7ea5ee73a0f3cbc
Bread Genie <msuhailbh07@gmail.com>
2026-01-29 18:26:11 +0530

    ci: failfast

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
 	workers: 1,
+	maxFailures: process.env.CI ? 5 : 0,
 	reporter: process.env.CI
 		? [
 				["github"],
