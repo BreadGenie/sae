28838419c2e07cf39eb7282d5fa2b53a3572133f
Bread Genie <msuhailbh07@gmail.com>
2026-04-14 12:40:20 +0530

    test: run with just one worker

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 1 : 0,
-	workers: process.env.CI ? 2 : 1,
+	workers: 1,
 	timeout: 45_000,
 	expect: {
 		timeout: 8_000,
