7adc80b395ba8f8e89daa4d898bdd962d8411c09
Bread Genie <msuhailbh07@gmail.com>
2026-04-15 10:40:22 +0530

    test: failfast with max 3 failures

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 1 : 0,
 	workers: process.env.CI ? 1 : 2,
+    maxFailures: process.env.CI ? 3 : undefined,
 	timeout: 60_000,
 	expect: {
 		timeout: 10_000,
