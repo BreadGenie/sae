12845dbe3c558867ffd0d7c2e2432c90fc9fc25b
Bread Genie <msuhailbh07@gmail.com>
2026-04-15 10:40:36 +0530

    test: unskip the test

--- e2e/specs/multi-participant.spec.ts
+++ e2e/specs/multi-participant.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, joinFromPreview } from "../fixtures/test";
 
-test.skip("Multi participant", () => {
+test.describe("Multi participant", () => {
 	test("host and two guests see the same meeting", async ({ hostPage, meetingId, createParticipant }) => {
 		const guestOne = await createParticipant();
 		const guestTwo = await createParticipant();
