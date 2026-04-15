7b3a2f635fb65dcca664a1de504526b733918546
Bread Genie <msuhailbh07@gmail.com>
2026-04-15 12:01:38 +0530

    Revert "ci: run test on 2 workers"
    This reverts commit a315c837532eb919c2a7f082cef39ee5b3101feb.

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	fullyParallel: false,
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 1 : 0,
-	workers: 2,
+	workers: process.env.CI ? 1 : 2,
     maxFailures: process.env.CI ? 3 : undefined,
 	timeout: 60_000,
 	expect: {
