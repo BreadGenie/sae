126064cf5b628b148a462d66443998ebb10f4d20
Bread Genie <msuhailbh07@gmail.com>
2026-04-15 11:16:46 +0530

    test: properly clear rate limit value

--- meet/api/test_helpers.py
+++ meet/api/test_helpers.py
@@ -7,5 +7,8 @@ from frappe.tests.utils import whitelist_for_tests
 
 @whitelist_for_tests()
 def clear_create_rate_limit() -> None:
-	"""Clear meeting creation rate limit cache. Only available in test/CI environments."""
-	frappe.cache.delete_keys("rl:meet.api.meeting.join_meeting_as_guest:*")
+	"""Clear meeting creation, join meeting as guest rate limit cache."""
+	keys = frappe.cache.get_keys("rl:meet.api.meeting.join_meeting_as_guest:*")
+	keys += frappe.cache.get_keys("rl:meet.api.meeting.create:*")
+	for key in keys:
+		frappe.cache.set(key, 0)
