4fb649906c2d2ec5b09bd42de6583d023aa1c348
Harshit Raizada <raizadaharshit2004@gmail.com>
2026-05-30 01:01:07 +0530

    chore: formatting

--- frontend/src/composables/usePollStore.ts
+++ frontend/src/composables/usePollStore.ts
@@ -1,39 +1,37 @@
 import { defineStore } from "pinia";
 import { computed, ref } from "vue";
 import type { PollPayloadFE } from "../../../sfu-server/src/types";
-export const usePollStore = defineStore("poll", ()=>{
-    const polls = ref<Record<string, PollPayloadFE>>({})
+export const usePollStore = defineStore("poll", () => {
+	const polls = ref<Record<string, PollPayloadFE>>({});
 
-    const activePolls = computed(()=>{
-        return Object.values(polls.value).filter(poll => poll.isActive);
-    })
+	const activePolls = computed(() => {
+		return Object.values(polls.value).filter((poll) => poll.isActive);
+	});
 
-    function addPoll(poll: PollPayloadFE) {
-        polls.value[poll.pollId] = poll;
-    }
+	function addPoll(poll: PollPayloadFE) {
+		polls.value[poll.pollId] = poll;
+	}
 
-    function updatePoll(poll: PollPayloadFE) {
-        polls.value[poll.pollId] = poll;
-    }
+	function updatePoll(poll: PollPayloadFE) {
+		polls.value[poll.pollId] = poll;
+	}
 
-    function setExistingPolls(existingPolls: PollPayloadFE[]) {
-        existingPolls.forEach(poll => {
-            polls.value[poll.pollId] = poll;
-        });
-    }
+	function setExistingPolls(existingPolls: PollPayloadFE[]) {
+		existingPolls.forEach((poll) => {
+			polls.value[poll.pollId] = poll;
+		});
+	}
 
-    function $reset() {
-        polls.value = {};
-    }
+	function $reset() {
+		polls.value = {};
+	}
 
-    return {
-        polls,
-        activePolls,
-        addPoll,
-        updatePoll,
-        setExistingPolls,
-        $reset,
-    };
-
-
-})
+	return {
+		polls,
+		activePolls,
+		addPoll,
+		updatePoll,
+		setExistingPolls,
+		$reset,
+	};
+});
